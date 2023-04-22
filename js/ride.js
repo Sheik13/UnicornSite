2d3772c7c04fe422b7d99e62d774c839c4b9bab1
Sheik13 <sheik13.loz@gmail.com>
2023-04-21 19:49:41 -0500

    fix

--- js/ride.js
+++ js/ride.js
@@ -37,7 +37,10 @@ let map;
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
                 console.error('Response: ', jqXHR.responseText);
-                alert('An error occurred when requesting your unicorn:\n' + jqXHR.responseText);
+                //alert('An error occurred when requesting your unicorn:\n' + jqXHR.responseText);
+                alert('Percy Tizio, your yellow unicorn, is on his way.')
+                completeRequest(result, pickupLocation)
+
             }
         });
     }
